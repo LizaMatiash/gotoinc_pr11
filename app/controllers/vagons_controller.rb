a01d43ebc6d84fb91f8cb22695b3143aacf58c10
LizaMatiash <liza.mymailbox@gmail.com>
2022-08-12 17:37:05 +0300

    Vagons Controller

---
+++ app/controllers/vagons_controller.rb
@@ -0,0 +1,57 @@
+class VagonsController < ApplicationController
+  before_action :set_vagon, only: %i[ show edit update destroy ]
+
+  def index
+    @vagons = Vagon.all
+  end
+
+  def show
+  end
+
+  def new
+    @vagon = Vagon.new
+  end
+
+  def edit
+  end
+
+  def create
+    @vagon = Vagon.new(vagon_params)
+
+    respond_to do |format|
+      if @vagon.save
+        format.html { redirect_to vagon_url(@vagon), notice: "Vagon was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @vagon.update(vagon_params)
+        format.html { redirect_to vagon_url(@vagon), notice: "Vagon was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @vagon.destroy
+
+    respond_to do |format|
+      format.html { redirect_to vagons_url, notice: "Vagon was successfully destroyed." }
+    end
+  end
+
+  private
+
+  def set_vagon
+    @vagon = Vagon.find(params[:id])
+  end
+
+  def vagon_params
+    params.require(:vagon).permit(:train_id, :type, :places)
+  end
+end
