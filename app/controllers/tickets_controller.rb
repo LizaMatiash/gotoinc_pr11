91c664f958d8bc358efa94a393bbc9b1bbc4c6e1
LizaMatiash <liza.mymailbox@gmail.com>
2022-09-01 09:23:44 +0300

    C3

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,17 +11,17 @@ class TicketsController < ApplicationController
   def new
     # byebug
     @ticket = Ticket.new
-    @base_station = RailwayStation.find(params[:base_station])
-    @end_station = RailwayStation.find(params[:end_station])
+    # @start_station = RailwayStation.find(params[:base_station])
+    # @end_station = RailwayStation.find(params[:end_station])
   end
 
   def create
     @ticket = @train.tickets.build(ticket_params)
     @ticket.user = User.first
-    # @ticket.base_station_id = 1
-    # @ticket.end_station_id = 3
+    @ticket.base_station_id = 1
+    @ticket.end_station_id = 3
     @ticket.firstname = 'As'
-    # byebug
+    byebug
     if @ticket.save
       redirect_to tickets_path, notice: 'Ticket was successfully created.'
     else
