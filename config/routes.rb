5200caf67c8e9227b4cc82a42306cad66f5f5278
LizaMatiash <liza.mymailbox@gmail.com>
2022-09-01 17:17:09 +0300

    Tickets + admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :trains do
     resources :vagons, shallow: true
     resources :tickets, shallow: true
   end
-  resources :railway_stations do
-    patch :update_position, on: :member
-    patch :update_time, on: :member
-  end
-  resources :routes
+
   resources :tickets, only: [:index]
   resource :search_ticket, only: [:new, :show, :edit] do
     post '/', to: 'search_tickets#find'
   end
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member
+      patch :update_time, on: :member
+    end
+    resources :routes
+    resources :tickets
+  end
+
   # resources :vagons
   get 'welcome/index'
 
